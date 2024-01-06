516605bde198e0c0ced0cad8aad2bcffd8ff4ebc
yepsan95 <56332181+yepsan95@users.noreply.github.com>
2024-01-06 17:18:52 -0500

    fix: handled error exceptions.

--- console.py
+++ console.py
@@ -115,20 +115,21 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        try:
+            if not args:
+                raise SyntaxError()
+            args_list = args.split(" ")
+            kw = {}
+            for arg in args_list[1:]:
+                arg_split = arg.split("=")
+                arg_split[1] = eval(arg_split[1])
+                if type(arg_split[1]) is str:
+                    arg_split[1] = arg_split[1].replace("_", " ").replace('"', '\\"')
+                kw[arg_split[0]] = arg_split[1]
+        except SyntaxError:
             print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
+        except NameError:
             print("** class doesn't exist **")
-            return
-        args_list = args.split(" ")
-        kw = {}
-        for arg in args_list[1:]:
-            arg_split = arg.split("=")
-            arg_split[1] = eval(arg_split[1])
-            if type(arg_split[1]) is str:
-                arg_split[1] = arg_split[1].replace("_", " ").replace('"', '\\"')
-            kw[arg_split[0]] = arg_split[1]
         new_instance = HBNBCommand.classes[args_list[0]](**kw)
         new_instance.save()
         print(new_instance.id)
