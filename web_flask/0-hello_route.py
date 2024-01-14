67b549bb18726ddecfe5028ed38e63fe52149943
Bryan Yep <yepsan95@gmail.com>
2024-01-13 23:47:18 -0500

    fix: documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Starts a Flask web application """
+"""Starts a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
-    """ / route """
+    """Access the root directory and prints a message"""
     return "Hello HBNB!"
 
 if __name__ == '__main__':
