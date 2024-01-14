47a58fc4a3f4920f54ef0984646dd95ecc0cac99
Bryan Yep <yepsan95@gmail.com>
2024-01-14 17:10:33 -0500

    fix: pycode style

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,15 +4,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
     """Access the root directory"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
     """Access the /hbnb directory"""
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
@@ -20,12 +23,14 @@ def c(text):
     text = text.replace("_", " ")
     return f"C {text}"
 
+
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
     """Access the /pyhton/<text> directory"""
     text = text.replace("_", " ")
     return f"Python {text}"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,21 +4,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
     """Access the root directory"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
     """Access the /hbnb directory"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """Access the /c<text> directory"""
     text = text.replace("_", " ")
     return f"C {text}"
+
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
@@ -27,10 +31,12 @@ def python(text="is cool"):
     text = text.replace("_", " ")
     return f"Python {text}"
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """Access the /number/<int:n> directory"""
     return f"{n} is a number"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
