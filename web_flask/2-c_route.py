7b98c4ae09b8349957b0e3587d5b7375e4145809
Bryan Yep <yepsan95@gmail.com>
2024-01-14 00:40:18 -0500

    feat: added script that starts a Flask web application.

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Access the root directory"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """Access the /hbnb directory"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Access the /c<text> directory"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
