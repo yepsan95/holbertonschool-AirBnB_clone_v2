a1a4fddcda92d44d41ef837465ce9bfa1dde5e51
Bryan Yep <yepsan95@gmail.com>
2024-01-14 15:21:43 -0500

    feat: script that starts a Flask web application

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Access the root directory"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """Access the /hbnb directory"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Access the /c<text> directory"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Access the /pyhton/<text> directory"""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Access the /number/<int:n> directory"""
+    return f"{n} is a number"
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Access the /number_template/<int:n> directory"""
+    return render_template('5-number.html', n=n)
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """Access the /number_odd_or_even/<int:n> directory"""
+    if n % 2 == 0:
+        odd_or_even = "even"
+    else:
+        odd_or_even = "odd"
+    return render_template('6-number_odd_or_even.html', n=n, odd_or_even=odd_or_even)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
