5b03466378a7d0e12814601c666e7821bd351bc9
Bryan Yep <yepsan95@gmail.com>
2024-01-18 13:25:33 -0500

    fix: added new public instance method delete() and updated all().

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            cls_objects = {}
+            for obj in FileStorage.__objects:
+                if obj.split('.')[0] == cls:
+                    cls_objects[obj] = FileStorage.__objects[obj]
+        return cls_objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +55,16 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes an object from __objects"""
+        if obj is None:
+            pass
+        else:
+            if obj in FileStorage.__objects:
+                FileStorage.__objects.pop(obj)
+
+    def close(self):
+        """Call reload method for deserializing
+        the JSON file to objects"""
+        self.reload()
